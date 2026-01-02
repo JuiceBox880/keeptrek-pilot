281cd10a99a471e5bbbdeeca6800ba630f4f1fc2
JuiceBox880 <stephen@whitefieldschurch.com>
2026-01-02 11:11:49 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 from dataclasses import dataclass, field
-from typing import Optional, List, Dict, Tuple
 import gspread
 from google.oauth2.service_account import Credentials
 
